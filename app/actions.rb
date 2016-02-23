4a48c91ee20b8b720970f030a6753121c2facad4
coreyholmes <coreyholmes20@gmail.com>
2016-02-23 04:50:08 +0000

    Added login/logout

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,77 @@
+helpers do 
+  def current_user
+    if session[:user_id]
+      Users.find(session[:user_id])
+    end
+  end
+end
+
 # Homepage (Root path)
 get '/' do
   @songs = Songs.all
-  if session[:vote]
-      session[:vote] += 1
-    else
-      session[:vote] = 0
-  end
+  @add_music = "display"
+  session[:vote] = 0
+  # if session[:vote]
+  #     session[:vote] += 1
+  #   else
+  #     session[:vote] = 0
+  # end
   erb :index
+end
+
+# Voting
+get '/upvote' do
+@upvoted = false
+  if @upvoted == false
+    session[:vote] += 1
+    @upvoted = true
+    erb :index
+  else
+    erb :index
+  end
+end
+
+# Register User
+get '/register' do
+  @login_btn = "display"
+  erb :'users/register'
+end
+
+post '/new-user' do
+  @users = Users.new(
+    first_name:   params[:first_name],
+    last_name: params[:last_name],
+    email:  params[:email],
+    password:  params[:password]
+  )
+  if @users.save
+    redirect '/'
+  else
+    erb :'users/register'
+  end
+end
+
+# Login User
+get '/login' do
+  erb :'users/login'
+end
+
+post '/login' do
+  @email = params[:email]
+  @password = params[:password]
+  @user = Users.where(email: @email, password: @password).first
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :'users/login'
+  end
+end
+
+# Logout User
+get '/logout' do
+  session.clear
+  redirect '/register'
 end
 
 # Adding new songs to the wall
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222111900) do
+ActiveRecord::Schema.define(version: 20160222172500) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
@@ -21,4 +21,20 @@ ActiveRecord::Schema.define(version: 20160222111900) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
+
+
+
+# votes
+
+# reference user_id
+# reference song_id
